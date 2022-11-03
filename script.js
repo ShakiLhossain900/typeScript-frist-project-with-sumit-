1663cc7d9c2f4c820712990039f9b8a82ddae387
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-03 22:52:00 +0600

    adding funtion

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const coutery = "Bangladesh";
 console.log(coutery);
 let name = 'this is shakil';
 console.log(name);
+function add(a, b) {
+    return a + b;
+}
+const result = add(2, 4);
+console.log(result);
